40901d474384789b66570acbee98eb3f9646fc89
LoiaFede <55881851+LoiaFede@users.noreply.github.com>
2019-11-28 02:37:13 -0300

    Primera subida

---
+++ inc/header.php
@@ -0,0 +1,9 @@
+<header id="header" class="">
+	<nav>
+		<ul>
+			<li><a href="#Sobremi" title="">Sobre mí</a></li>
+			<li><a href="#Proyectos" title="">Proyectos</a></li>
+			<li><a href="#Contactos" title="">Contacto</a></li>
+		</ul>
+	</nav>
+</header><!-- /header -->
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link href="https://fonts.googleapis.com/css?family=Courgette|Kaushan+Script&display=swap" rel="stylesheet"> 
+	<title>Federico Lo Iacono</title>
+	<link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+	<div class="contenedorgral">
+
+	<?php include 'inc/header.php'; ?>
+
+	<img id="Logo" src="img/Logo2.png">
+	<div class=textointro>
+		<h1>¡Bienvenido a mi portofolio!</h1>
+	</div>
+	
+	</div>
+	<!-- Inicio ParteSobremi -->
+	<section id="Sobremi">
+		Estoasassdasadsdad
+	</section>
+	<!-- Fin de Parte Sobremi -->
+
+	<!-- Inicio ParteTrabajos -->
+
+	<!-- Fin de ParteTrabajos -->
+
+	<!-- Inicio ParteContacto -->
+
+	<!-- Fin de ParteContacto -->
+
+</body>
+</html>
