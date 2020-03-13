f8b13c8dda3f4a3025605147853078089c4ccd14
LoiaFede <55881851+LoiaFede@users.noreply.github.com>
2020-03-13 18:39:57 -0300

    Respaldo

--- inc/header.php
+++ inc/header.php
@@ -1,9 +1,9 @@
 <header id="header" class="">
 	<nav>
 		<ul>
-			<li><a href="#Sobremi" title="">Sobre mí</a></li>
-			<li><a href="#Proyectos" title="">Proyectos</a></li>
-			<li><a href="#Contactos" title="">Contacto</a></li>
+			<li><a href="#Sobremigral" title="">Sobre mí</a></li>
+			<li><a href="#Proyectosgral" title="">Proyectos</a></li>
+			<li><a href="#Contactosgral" title="">Contacto</a></li>
 		</ul>
 	</nav>
 </header><!-- /header -->
--- index.php
+++ index.php
@@ -6,6 +6,17 @@
 	<link href="https://fonts.googleapis.com/css?family=Courgette|Kaushan+Script&display=swap" rel="stylesheet"> 
 	<title>Federico Lo Iacono</title>
 	<link rel="stylesheet" href="css/estilos.css">
+	
+	<script src="node_modules/jquery/dist/jquery.js"></script>
+	<script	src="js/jquery.scrollUp.js"></script>
+	<script>
+			$(function(){
+	  		$.scrollUp({
+	  			scrollImg: true
+	  		});
+			});
+	</script>
+
 </head>
 <body>
 	<div class="contenedorgral">
@@ -19,18 +30,34 @@
 	
 	</div>
 	<!-- Inicio ParteSobremi -->
-	<section id="Sobremi">
-		Estoasassdasadsdad
+	<section id="Sobremigral">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 	</section>
 	<!-- Fin de Parte Sobremi -->
 
 	<!-- Inicio ParteTrabajos -->
-
+	<section id="Proyectosgral">
+		
+	</section>
 	<!-- Fin de ParteTrabajos -->
 
 	<!-- Inicio ParteContacto -->
-
+	<section id="Contactosgral">
+		
+	</section>
 	<!-- Fin de ParteContacto -->
+	<section id=Fin>
+		
+	</section>
+
+<script>
+	
+</script>
 
 </body>
 </html>
